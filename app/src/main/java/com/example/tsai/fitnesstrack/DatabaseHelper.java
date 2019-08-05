9356aac8614ad7e0abecce465cf81c19f4e3d38b
ftsai231 <f84024048@gmail.com>
2019-08-05 13:47:29 -0400

    more database feature

--- app/src/main/java/com/example/tsai/fitnesstrack/DatabaseHelper.java
+++ app/src/main/java/com/example/tsai/fitnesstrack/DatabaseHelper.java
@@ -2,27 +2,26 @@ package com.example.tsai.fitnesstrack;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "workout.db";
+    public static final String DATABASE_NAME = "workoutDB";
     public static final String TABLE_NAME = "workout_table";
-    public static final String Col_1 = "Set";
-    public static final String Col_2 = "Weight";
-    public static final String Col_3 = "reps";
+    public static final String Col_1 = "SETT";
+    public static final String Col_2 = "WEIGHT";
+    public static final String Col_3 = "REPS";
 
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
-
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT,MARKS INTEGER)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME +" (SETT INTEGER PRIMARY KEY AUTOINCREMENT, WEIGHT TEXT , REPS TEXT);");
     }
 
     @Override
@@ -38,9 +37,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(Col_2, weight);
         contentValues.put(Col_3, reps);
         long result = db.insert(TABLE_NAME, null, contentValues);
+        System.out.print(result);
         if(result==-1){
             return false;
         }
-        return true;
+        else return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- app/src/main/java/com/example/tsai/fitnesstrack/WorkoutPlan_Activity.java
+++ app/src/main/java/com/example/tsai/fitnesstrack/WorkoutPlan_Activity.java
@@ -1,10 +1,12 @@
 package com.example.tsai.fitnesstrack;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WorkoutPlan_Activity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class WorkoutPlan_Activity extends AppCompatActivity {
     Button updateSetBtn;
     EditText editWeight;
     EditText editReps;
+    TextView workoutTable;
 
 
     @Override
@@ -23,12 +26,19 @@ public class WorkoutPlan_Activity extends AppCompatActivity {
         setContentView(R.layout.workoutplan);
         myDb = new DatabaseHelper(this);
 
-        editWeight = findViewById(R.id.weight);
-        editReps = findViewById(R.id.reps);
+        editWeight = (EditText) findViewById(R.id.weight);
+        editReps = (EditText) findViewById(R.id.reps);
         addSetBtn = (Button) findViewById(R.id.add);
         deleteSetBtn = (Button) findViewById(R.id.delete);
         updateSetBtn = (Button) findViewById(R.id.update);
+        workoutTable = (TextView) findViewById(R.id.table);
 
+        addData();
+
+
+    }
+
+    public void addData(){
         addSetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,14 +49,21 @@ public class WorkoutPlan_Activity extends AppCompatActivity {
                 else{
                     Toast.makeText(WorkoutPlan_Activity.this, "Set not Inserted", Toast.LENGTH_LONG).show();
                 }
+                String str = viewData();
+                workoutTable.setText(str);
             }
         });
+    }
 
-
-
-
-
-
+    public String viewData(){
+        Cursor res = myDb.getAllData();
+        StringBuilder sb = new StringBuilder();
+        while(res.moveToNext()){
+            sb.append("Set: " + res.getString(0)+"   ");
+            sb.append("Weight: " + res.getString(1) + "   ");
+            sb.append("Reps: " + res.getString(2) + "\n");
+        }
+        return sb.toString();
 
     }
 
